eca8db8fe875902a9fa056989f90b5f3e33190fa
Nico Nel <niconel@gmail.com>
2025-08-23 20:01:55 +0200

    header improvements

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -68,7 +68,7 @@ export function Header() {
   const [isOpen, setIsOpen] = React.useState(false);
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background ">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <Sheet open={isOpen} onOpenChange={setIsOpen}>
           <SheetTrigger asChild>
