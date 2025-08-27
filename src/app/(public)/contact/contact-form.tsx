53dfb9f34b8746d6dd64774dde586cadd3b4e9aa
Nico Nel <niconel@gmail.com>
2025-08-27 20:33:16 +0200

    start of contact form

---
+++ src/app/(public)/contact/contact-form.tsx
@@ -0,0 +1,101 @@
+"use client";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  email: z.string().email(),
+  message: z.string().min(1),
+});
+
+export default function ContactUsForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      console.log(values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 w-full "
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Your Name</FormLabel>
+              <FormControl>
+                <Input placeholder=" " type="" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email Address</FormLabel>
+              <FormControl>
+                <Input placeholder="" type="email" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell us what is on your mind"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -1,5 +1,28 @@
+import { Smile } from "lucide-react";
+import ContactUsForm from "./contact-form";
+
 const Page = () => {
-  return <div>Contact Page</div>;
+  return (
+    <div className=" grid flex-1 lg:grid-cols-2">
+      <div className="hidden flex-1 items-center justify-end p-6 md:p-10 lg:flex">
+        <ul className="max-w-sm space-y-8">
+          <li>
+            <div className="flex items-center gap-2">
+              <Smile className="size-4" />
+              <p className="font-semibold">Let&apos;s Start a Conversation!</p>
+            </div>
+            <p className="text-muted-foreground mt-2 text-sm">
+              We&apos;d love to hear from you. Send us a message and we&apos;ll
+              respond as soon as possible.
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-1 items-center justify-center p-6 md:p-10 lg:justify-start">
+        <ContactUsForm />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
